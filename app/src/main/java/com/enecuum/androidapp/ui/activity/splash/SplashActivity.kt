e6df93843f6742936d8ae715a20651c89c34f5ea
Oleg Maximov <syjgin@enecuum.com>
2018-01-24 19:11:35 +0300

    real layout started

--- app/src/main/java/com/enecuum/androidapp/ui/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/enecuum/androidapp/ui/activity/splash/SplashActivity.kt
@@ -3,12 +3,16 @@ package com.enecuum.androidapp.ui.activity.splash
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.graphics.drawable.AnimationUtilsCompat
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import base_ui_primitives.BaseActivity
 
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.enecuum.androidapp.R
 import com.enecuum.androidapp.presentation.view.splash.SplashView
 import com.enecuum.androidapp.presentation.presenter.splash.SplashPresenter
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity : BaseActivity(), SplashView {
@@ -24,6 +28,7 @@ class SplashActivity : BaseActivity(), SplashView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        loading.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate))
         presenter.onCreate()
     }
 }
