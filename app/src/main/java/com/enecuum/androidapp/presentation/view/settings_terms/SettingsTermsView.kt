0918f7569b118ebccf294c5c59afc7195a80c661
Oleg Maximov <syjgin@enecuum.com>
2018-02-02 13:58:30 +0300

    settings terms screen

--- app/src/main/java/com/enecuum/androidapp/presentation/view/settings_terms/SettingsTermsView.kt
+++ app/src/main/java/com/enecuum/androidapp/presentation/view/settings_terms/SettingsTermsView.kt
@@ -1,7 +1,9 @@
 package com.enecuum.androidapp.presentation.view.settings_terms
 
+import android.text.Spanned
 import com.arellomobile.mvp.MvpView
 
 interface SettingsTermsView : MvpView {
+    fun displayTerms(termsText: Spanned?)
 
 }
--- app/src/main/java/com/enecuum/androidapp/ui/fragment/settings_terms/SettingsTermsFragment.kt
+++ app/src/main/java/com/enecuum/androidapp/ui/fragment/settings_terms/SettingsTermsFragment.kt
@@ -1,6 +1,7 @@
 package com.enecuum.androidapp.ui.fragment.settings_terms
 
 import android.os.Bundle
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import com.enecuum.androidapp.presentation.presenter.settings_terms.SettingsTerm
 
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.enecuum.androidapp.ui.base_ui_primitives.BackTitleFragment
+import kotlinx.android.synthetic.main.fragment_settings_terms.*
 
 class SettingsTermsFragment : BackTitleFragment(), SettingsTermsView {
     companion object {
@@ -31,10 +33,19 @@ class SettingsTermsFragment : BackTitleFragment(), SettingsTermsView {
         return inflater.inflate(R.layout.fragment_settings_terms, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.onCreate()
+    }
+
     override fun onResume() {
         super.onResume()
         menu?.clear()
     }
 
     override fun getTitle(): String = getString(R.string.terms)
+
+    override fun displayTerms(termsText: Spanned?) {
+        terms.text = termsText
+    }
 }
