e6aac043bbf390a02349eea8071283ca10ff3dfd
Kosyakov Stanislav <steam0111@mail.ru>
2018-10-15 12:20:10 +0300

    Settings screen about app, updated link

--- app/src/main/java/com/enecuumwallet/androidapp/application/Constants.kt
+++ app/src/main/java/com/enecuumwallet/androidapp/application/Constants.kt
@@ -3,7 +3,7 @@ package com.enecuumwallet.androidapp.application
 interface Constants {
 
     object AppConstants {
-        const val URL_WHITE_PAPER = "https://new.enecuum.com/files/pp_en.pdf"
+        const val URL_WHITE_PAPER = "https://enecuum.com/docs/pp_en.pdf"
         const val URL_PRIVACY = "https://enecuum.com/docs/privacy.pdf"
         const val URL_TERMS = "https://enecuum.com/docs/terms.pdf"
         const val URL_WEB_SITE = "https://enecuum.com"
