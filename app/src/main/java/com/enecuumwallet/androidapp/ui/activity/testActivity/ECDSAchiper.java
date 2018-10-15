427b59528992a7e5974739d4a3480e7e0ddfa92d
Kosyakov Stanislav <steam0111@mail.ru>
2018-10-15 12:53:43 +0300

    Updted version

--- app/src/main/java/com/enecuumwallet/androidapp/ui/activity/testActivity/ECDSAchiper.java
+++ app/src/main/java/com/enecuumwallet/androidapp/ui/activity/testActivity/ECDSAchiper.java
@@ -1,14 +1,21 @@
 package com.enecuumwallet.androidapp.ui.activity.testActivity;
 
 import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
 import java.security.SecureRandom;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.spec.ECGenParameterSpec;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 public class ECDSAchiper {
 
@@ -34,4 +41,18 @@ public class ECDSAchiper {
     public static boolean isEven(BigInteger number)  {
            return number.getLowestSetBit() != 0;
   }
+
+    public static byte[] signData(byte[] data, byte[] privateKey)
+            throws GeneralSecurityException {
+        Signature signer = Signature.getInstance("SHA256WithRSA", new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        signer.initSign(keyToValue(privateKey));
+        signer.update(data);
+        return signer.sign();
+    }
+
+    private static PrivateKey keyToValue(byte[] pkcs8key) throws GeneralSecurityException, NoSuchAlgorithmException {
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(pkcs8key);
+        KeyFactory factory = KeyFactory.getInstance("ECDSA");
+        return factory.generatePrivate(spec);
+    }
 }
